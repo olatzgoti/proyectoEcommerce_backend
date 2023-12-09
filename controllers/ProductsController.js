8fee909ba86c354c0735ca27194200c6318dd79c
olatz <gotiolatz@gmail.com>
2023-12-09 20:02:26 +0100

    getBy Price+getAllconCat+validacionCreateproduct

--- controllers/ProductsController.js
+++ controllers/ProductsController.js
@@ -1,4 +1,4 @@
-const { Product, Sequelize} = require('../models/index.js');
+const { Category, Product, Sequelize} = require('../models/index.js');
 
 const {Op} = Sequelize;
 
@@ -6,13 +6,25 @@ const {Op} = Sequelize;
 
 const ProductController = {
     create(req, res) {
+      const name = req.body.name;
+      const price = req.body.price;
+      const category = req.body.CategoryId;
+      console.log(req.body);
+    
+      if(!name || !price || !category) 
+      { 
+        res.send('Rellene todos los campos');
+    }
+      else
+      {
       Product.create(req.body)
       .then(product => res.status(201).send({message: 'Product created', product}))
       .catch((error) => console.error(error))
-    },
+}},
   
     async update(req, res) {//SOLO DE NOMBRE LO 1º, falta modificar otros campos
       await Product.update(
+      //  const {name, CategoryId} = 
         {name: req.body.name}, {where: {id: req.params.id}})
         .then(res.status(200).send({message: 'Product updated'}))
         .catch((error) => console.error(error))
@@ -25,11 +37,14 @@ const ProductController = {
       .catch((error) => console.error(error))
     },
   
-    async getAll(req, res) {//APARECEN SIN LA CATEGORIA, PENDIENTE DE AÑADIR
-      await Product.findAll()
-      .then(products => res.status(200).send({message: 'Products:', products}))
+
+    async getAll(req, res) {
+      await Product.findAll({include: [Category]})
+      .then(products => res.status(200).send({message: 'Products:', products
+    }))
       .catch((error) => console.error(error))
     },
+    
   
     async getById(req, res) {
       await Product.findByPk(req.params.id)
@@ -43,8 +58,25 @@ const ProductController = {
       })
       .then(product => res.status(200).send({product}))
       .catch((error) => console.error(error))
+    },
+
+    async getByPrice(req,res){
+
+      await Product.findAll({
+        where: {price: {[Op.like]: req.params.price}}
+      })
+      .then(product=>res.status(200).send({product}))
+      .catch((error)=>console.log(error))
+    },
+
+    async orderByPrice(req, res){
+      
+        await Product.findAll({ where: { price: {[options.query.order]: price }}})
+        .then(products => res.status(200).send({message: 'Products:', products
+      }))
+        .catch((error) => console.error(error))
+      }     
     }
-    
-  }
+  
   
   module.exports = ProductController;
--- routes/products.js
+++ routes/products.js
@@ -7,13 +7,25 @@ router.post('/newProduct', ProductController.create);
 
 router.get('/getAll', ProductController.getAll);
 
+//router.get('/getAll?order=price', ProductController.getAll);
+///personas?order=-ci
+
+router.get('orderByPrice', ProductController.orderByPrice);
+
 router.get('/getById/id/:id', ProductController.getById);
 
 router.get('/getbyname/name/:name', ProductController.getByName);
 
+router.get('/getByPrice/:price', ProductController.getByPrice);
+
+router.get('/getAll?order=-id', ProductController.getAll);
+
 router.put('/update/id/:id', ProductController.update);
 
 router.delete(('/delete/id/:id'), ProductController.delete);
+
+
+
 
 
 module.exports = router;
